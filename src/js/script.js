39be225ca1d4da15461b6e6b619f1233bb4d6338
ingridFacens <ingrid.camargo_@hotmail.com>
2022-09-28 12:13:46 -0300

    add dropdown animation

--- src/js/script.js
+++
@@ -1,7 +0,0 @@
-const dropdownButtonDown = document.querySelector('#dropdownButtonDown');
-
-dropdownButtonDown.addEventListener('click', () => {
-    const dropdownText = document.querySelector('.dropdown__text--purple');
-    
-    dropdownText.classList.toggle('hide-for-all');
-})
